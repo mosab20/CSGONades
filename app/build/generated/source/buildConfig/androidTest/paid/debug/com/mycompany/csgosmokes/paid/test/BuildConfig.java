b4b47396d046777d754d48d645980e30441c38ec
mtohmaz2 <mtohmaz2@ncsu.edu>
2018-03-08 22:35:10 -0500

    Adding project to GitHub.

---
+++ app/build/generated/source/buildConfig/androidTest/paid/debug/com/mycompany/csgosmokes/paid/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mycompany.csgosmokes.paid.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mycompany.csgosmokes.paid.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "paid";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "1.0-paid";
+  // Fields from product flavor: paid
+  public static final boolean PAID_VERSION = true;
+}
---
+++ app/build/generated/source/buildConfig/paid/release/com/mycompany/csgonades/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mycompany.csgonades;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.mycompany.csgosmokes.paid";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "paid";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "1.0-paid";
+  // Fields from product flavor: paid
+  public static final boolean PAID_VERSION = true;
+}
